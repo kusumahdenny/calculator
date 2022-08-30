f2cdcff6b4a385fb2a8a6f2dd19b84636a701763
Dan Kusuma <kusumahdenny@gmail.com>
2022-08-30 19:46:42 +0700

    create addEventListenerfor each number buttons
    by changing number input into strings, all numbers will be appended instead of being added into display

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ class Calculator {
     constructor(currentOperandText, previousOperandText) {
         this.previousOperandText = previousOperandText
         this.currentOperandText = currentOperandText
+        this.clear()
     }
 
     clear() {
-
+        this.previousOperand = ''
+        this.currentOperand = ''
+        this.operation = undefined
     }
 
     delete() {
@@ -13,7 +16,7 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
@@ -25,15 +28,24 @@ class Calculator {
     }
 
     updateDisplay() {
-
+        this.currentOperandText.innerText = this.currentOperand
     }
 }
 
 
-const numberButton = document.querySelectorAll('[data-number]');
-const operationButton = document.querySelectorAll('[data-operation]');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const clearButton = document.querySelector('[data-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const previousOperandText = document.querySelector('[data-previous]');
 const currentOperandText = document.querySelector('[data-current]');
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
