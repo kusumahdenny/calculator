19e36ae0674150cf5220f8020891309d74576b7c
Dan Kusuma <kusumahdenny@gmail.com>
2022-08-30 20:33:14 +0700

    create addEventListenerfor delete button

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0,-1)
     }
 
     appendNumber(number) {
@@ -94,5 +94,10 @@ equalsButton.addEventListener('click', button => {
 
 clearButton.addEventListener('click', button => {
     calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
     calculator.updateDisplay()
 })
